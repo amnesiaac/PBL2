61d5fe3a943254f5ce9208b1a6fe5a3494130238
amnesiaac <bruno.felipe130@gmail.com>
2019-04-01 22:11:18 -0300

    Pbl 2 faltando ocultar funcionários e moveis indisponiveis

---
+++ PBL2/Models/Venda.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PBL2.Models
+{
+    public class Venda
+    {
+        [Key]
+        public int Pk_Venda { get; set; }
+        [ForeignKey("Funcionario")]
+        public int Fk_Funcionario { get; set; }
+        public Funcionario Funcionario { get; set; }
+        [ForeignKey("Movel")]
+        public int Fk_Movel { get; set; }
+        public Movel Movel { get; set; }
+
+        public void setstatusfuncionario()
+        {
+            Funcionario.Status = "Indisponivel";
+        }
+        public void setstatusmovel()
+        {
+            Movel.Status = "Em construção";
+
+        }
+
+    }
+}
